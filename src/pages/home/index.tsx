87e285ea1a53f22f27cadffc5dd872818057b050
Cinthia Andrade <cinthia.andrade@insightlab.ufc.br>
2021-07-09 21:25:44 -0300

    chore[📝]: initial part of the home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,16 @@ import './styles.scss';
 const Home: React.FC = () => {
   return (
     <section className="container__home" id="home">
-      <h1>OLA</h1>
+      <div className="home-left">
+        <h1>
+          Olá! <strong>Eu sou a</strong> Cinthia Andrade
+        </h1>
+        <p>
+          Sou desenvolvedora front end há 2 anos, apaixonada por tecnologia e
+          transformação digital
+        </p>
+      </div>
+      <div className="home-right"></div>
     </section>
   );
 };
